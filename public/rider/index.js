0365f7d4a66f7c2fc24641a1f2d740e6c18559a3
AggrehO <richardsonlyon911@gmail.com>
2021-02-08 14:27:06 +0100

    completed the screen to select where user would like to order from

---
+++ public/rider/index.js
@@ -0,0 +1,5 @@
+// materialize initialization
+ document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.dropdown-trigger');
+    M.Dropdown.init(elems);
+});
